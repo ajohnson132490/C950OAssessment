9c10caf53cd7f5352b7d863bb5ba80e22000b053
Austin Johnson <ajohnson132490@gmail.com>
2024-12-06 08:56:08 -0600

    user interface and mileage report

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ from Package import Package
 from Truck import Truck
 from datetime import datetime, timedelta
 
-def main():
-    hash = Hash()
+# Create the hashtable
+hash = Hash()
 
+# Create the trucks
+truck1 = Truck(1)
+truck2 = Truck(2)
+
+def main():
     # Get all the packages in the hash
     with open('Resources/packages.csv') as package_file:
         reader = csv.reader(package_file, delimiter = ",")
@@ -36,10 +41,6 @@ def main():
     truck1_second_load = [6, 25, 11]
     truck2_second_load = [9, 10, 28, 32]
 
-    # Create the trucks
-    truck1 = Truck(1)
-    truck2 = Truck(2)
-
     # Load and deliver the first route
     truck1.loadPackages(hash, truck1_first_load)
     truck2.loadPackages(hash, truck2_first_load)
@@ -54,8 +55,59 @@ def main():
     truck1.deliverPackages(hash)
     truck2.deliverPackages(hash)
 
-    print(truck1.mileage)
+    #print(truck1.delivery_timestamps)
+    #print(str(truck1.delivery_timestamps[0][1]))
 
+    userInterface(hash)
+
+def userInterface(hash):
+    # Welcome screen
+    print("-----------------------------")
+    print("      WGU Parcel Service     ")
+    print("-----------------------------")
+
+    # Menu options
+    print("Please select one of the following options:")
+    print("\t 1. Delivery Report By Time")
+    print("\t 2. Delivery Report By Package")
+    print("\t 3. Truck Mileage")
+    print("\t 4. Exit")
+
+    # Generate report by option
+    option = None
+    while option is None: 
+        option = input()
+
+        match option:
+            case "1":
+                allPackagesReport()
+            case "2":
+                packageReport()
+            case "3":
+                mileageReport()
+            case "4":
+                print("Exit")
+                quit()
+            case default:
+                option = None
+                print("Invalid input, please try again.")
+            
+def allPackagesReport():
+    print("haha")
+        
+def packageReport():
+    print("haha")
+
+def mileageReport():
+    # Report welcome screen
+    print("-----------------------------")
+    print("        Truck Mileage        ")
+    print("-----------------------------")
+
+    # Pulling the data from the trucks. Truck 3 is never used
+    print(f"Truck {truck1.id} mileage: {truck1.mileage:.2f} miles")
+    print(f"Truck {truck2.id} mileage: {truck2.mileage:.2f} miles")
+    print("Truck 3 mileage: 0.00 miles")
 
 if __name__ == "__main__":
     main()
